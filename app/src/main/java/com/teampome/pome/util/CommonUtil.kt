41ade475d6a4da4790deb1176c2704ad98e2e1b5
hanseuk-Choi <hschoi5542@gmail.com>
2022-11-06 20:23:01 +0900

    feat : CommonUtil inputMode 추가

--- app/src/main/java/com/teampome/pome/util/CommonUtil.kt
+++ app/src/main/java/com/teampome/pome/util/CommonUtil.kt
@@ -2,6 +2,7 @@ package com.teampome.pome.util
 
 import android.app.Activity
 import android.content.Context
+import android.view.WindowManager.LayoutParams
 import android.view.inputmethod.InputMethodManager
 
 object CommonUtil {
@@ -19,5 +20,19 @@ object CommonUtil {
                 InputMethodManager.HIDE_NOT_ALWAYS
             )
         }
+    }
+
+    /**
+     *  keyboard가 화면을 밀게 설정
+     */
+    fun inputModeResize(activity: Activity) {
+        activity.window.setSoftInputMode(LayoutParams.SOFT_INPUT_ADJUST_PAN)
+    }
+
+    /**
+     *  keyboard가 화면을 못밀게 설정
+     */
+    fun inputModeNothing(activity: Activity) {
+        activity.window.setSoftInputMode(LayoutParams.SOFT_INPUT_ADJUST_NOTHING)
     }
 }
