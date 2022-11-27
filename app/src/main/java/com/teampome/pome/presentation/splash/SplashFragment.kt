50f5f71d9c4cf95f3b2390bc283334763caae1b5
hanseuk-Choi <hschoi5542@gmail.com>
2022-11-27 17:04:05 +0900

    feat : Spinner 지우고 Constraint Layout으로 변경(Spinner 사용을 하지 않아서)

--- app/src/main/java/com/teampome/pome/presentation/splash/SplashFragment.kt
+++ app/src/main/java/com/teampome/pome/presentation/splash/SplashFragment.kt
@@ -12,7 +12,7 @@ import kotlinx.coroutines.*
 
 class SplashFragment : BaseFragment<FragmentSplashBinding>(R.layout.fragment_splash){
 
-    private var isFirstLogin = true
+    private var isFirstLogin = false
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
