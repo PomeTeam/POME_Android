cb83768583a28d941d8c5aaa2bf9314ea890d68f
hanseuk-Choi <hschoi5542@gmail.com>
2022-11-30 21:06:14 +0900

    feat : splash & login 새로운 디자인 수정

--- app/src/main/java/com/teampome/pome/presentation/splash/SplashLoginFragment.kt
+++ app/src/main/java/com/teampome/pome/presentation/splash/SplashLoginFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.teampome.pome.R
 import com.teampome.pome.databinding.FragmentLoginSplashBinding
@@ -24,9 +25,19 @@ class SplashLoginFragment : BaseFragment<FragmentLoginSplashBinding>(R.layout.fr
     }
 
     override fun initListener() {
-        binding.loginKakaoButtonAiv.setOnClickListener {
+        binding.loginStartBtnAcb.setOnClickListener {
             // kakao 인증 후 register 화면으로 이동
             moveToRegister()
+        }
+        
+        // kakao login btn click
+        binding.loginKakaoBtnAiv.setOnClickListener {
+            Toast.makeText(requireContext(), "kakao login", Toast.LENGTH_SHORT).show()
+        }
+        
+        // google login btn click
+        binding.loginGoogleBtnAiv.setOnClickListener {
+            Toast.makeText(requireContext(), "google login", Toast.LENGTH_SHORT).show()
         }
     }
 
