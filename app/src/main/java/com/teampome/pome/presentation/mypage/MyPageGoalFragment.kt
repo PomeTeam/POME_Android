a9e5a4b85325c2c0b34987813280a341f77d4c06
sgsk88 <sgsk88@nate.com>
2022-12-28 15:42:21 +0900

    [feat] 마이탭 뷰 진행중

--- app/src/main/java/com/teampome/pome/presentation/mypage/MyPageGoalFragment.kt
+++ app/src/main/java/com/teampome/pome/presentation/mypage/MyPageGoalFragment.kt
@@ -3,9 +3,10 @@ package com.teampome.pome.presentation.mypage
 import com.teampome.pome.R
 import com.teampome.pome.util.base.BaseFragment
 import com.teampome.pome.databinding.FragmentMypageBinding
+import com.teampome.pome.databinding.FragmentMypageGoalBinding
 
-//마이페이지 탭
-class MyPageFragment : BaseFragment<FragmentMypageBinding>(R.layout.fragment_mypage) {
+//마이페이지 완료된 목표 뷰
+class MyPageGoalFragment : BaseFragment<FragmentMypageGoalBinding>(R.layout.fragment_mypage_goal) {
     override fun initListener() {
 
     }
