13ff04cb1b74f2d6e6c8a819a255852424c29e08
sgsk88 <sgsk88@nate.com>
2022-12-30 14:35:05 +0900

    [feat] 설정 -> 친구관리, 로그아웃 뷰 구현

---
+++ app/src/main/java/com/teampome/pome/presentation/mypage/MyPageFriendFragment.kt
@@ -0,0 +1,15 @@
+package com.teampome.pome.presentation.mypage
+
+import com.teampome.pome.R
+import com.teampome.pome.databinding.FragmentMypageAlarmBinding
+import com.teampome.pome.util.base.BaseFragment
+import com.teampome.pome.databinding.FragmentMypageBinding
+import com.teampome.pome.databinding.FragmentMypageFriendBinding
+import com.teampome.pome.databinding.FragmentMypageGoalBinding
+
+//마이페이지 친구관리 뷰
+class MyPageFriendFragment : BaseFragment<FragmentMypageFriendBinding>(R.layout.fragment_mypage_friend) {
+    override fun initListener() {
+
+    }
+}
---
+++ app/src/main/java/com/teampome/pome/presentation/mypage/MyPageSettingFragment.kt
@@ -0,0 +1,15 @@
+package com.teampome.pome.presentation.mypage
+
+import com.teampome.pome.R
+import com.teampome.pome.databinding.FragmentMypageAlarmBinding
+import com.teampome.pome.util.base.BaseFragment
+import com.teampome.pome.databinding.FragmentMypageBinding
+import com.teampome.pome.databinding.FragmentMypageGoalBinding
+import com.teampome.pome.databinding.FragmentMypageSettingBinding
+
+//마이페이지 설정 뷰
+class MyPageSettingFragment : BaseFragment<FragmentMypageSettingBinding>(R.layout.fragment_mypage_setting) {
+    override fun initListener() {
+
+    }
+}
