292bfc5db52305c1223c701c88328d4724c10863
hanseuk-Choi <hschoi5542@gmail.com>
2023-01-05 23:58:17 +0900

    feat : custom calendar 구현

--- app/src/main/java/com/teampome/pome/presentation/record/ModifyRecordCardFragment.kt
+++ app/src/main/java/com/teampome/pome/presentation/record/ModifyRecordCardFragment.kt
@@ -1,17 +1,27 @@
 package com.teampome.pome.presentation.record
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.DayViewFacade
+import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter
+import com.prolificinteractive.materialcalendarview.format.MonthArrayTitleFormatter
+import com.prolificinteractive.materialcalendarview.format.TitleFormatter
 import com.teampome.pome.R
 import com.teampome.pome.databinding.FragmentModifyRecordCardBinding
+import com.teampome.pome.databinding.PomeCalendarBottomSheetDialogBinding
 import com.teampome.pome.databinding.PomeTextListBottomSheetDialogBinding
 import com.teampome.pome.util.CommonUtil
 import com.teampome.pome.util.base.BaseFragment
+import org.threeten.bp.DayOfWeek
 import java.text.SimpleDateFormat
 import java.util.Date
 
@@ -19,8 +29,13 @@ class ModifyRecordCardFragment : BaseFragment<FragmentModifyRecordCardBinding>(R
 
     private val args: ModifyRecordCardFragmentArgs by navArgs()
 
+    // goal bottom sheet
     private lateinit var goalBottomSheetDialog: BottomSheetDialog
     private lateinit var goalBottomSheetDialogBinding: PomeTextListBottomSheetDialogBinding
+
+    // calendar bottom sheet
+    private lateinit var calendarBottomSheetDialog: BottomSheetDialog
+    private lateinit var calendarBottomSheetDialogBinding: PomeCalendarBottomSheetDialogBinding
 
     @SuppressLint("SimpleDateFormat")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -28,6 +43,7 @@ class ModifyRecordCardFragment : BaseFragment<FragmentModifyRecordCardBinding>(R
 
         binding.modifyRecordGoalAet.setText(args.currentCategory)
         makeGoalBottomSheetDialog()
+        makeCalendarBottomSheetDialog()
 
         // 일단 현재 시간으로...
         val now = System.currentTimeMillis()
@@ -51,6 +67,10 @@ class ModifyRecordCardFragment : BaseFragment<FragmentModifyRecordCardBinding>(R
         binding.modifyRecordDownArrowAiv.setOnClickListener {
             goalBottomSheetDialog.show()
         }
+
+        binding.modifyRecordCalenderAiv.setOnClickListener {
+            calendarBottomSheetDialog.show()
+        }
     }
 
     private fun makeGoalBottomSheetDialog() {
@@ -71,5 +91,51 @@ class ModifyRecordCardFragment : BaseFragment<FragmentModifyRecordCardBinding>(R
             })
         }
         goalBottomSheetDialogBinding.executePendingBindings()
+    }
+
+    private fun makeCalendarBottomSheetDialog() {
+        calendarBottomSheetDialog = BottomSheetDialog(requireContext())
+        calendarBottomSheetDialogBinding = PomeCalendarBottomSheetDialogBinding.inflate(layoutInflater, null, false)
+
+        calendarBottomSheetDialog.setContentView(calendarBottomSheetDialogBinding.root)
+
+        settingCustomCalendar()
+    }
+
+    private fun settingCustomCalendar() {
+        calendarBottomSheetDialogBinding.calendarMcv.apply {
+            // 첫 시작 요일 - 월요일
+            state().edit().setFirstDayOfWeek(DayOfWeek.MONDAY).commit()
+
+            // 한글 설정
+            setTitleFormatter(MonthArrayTitleFormatter(resources.getTextArray(R.array.custom_months)))
+            setWeekDayFormatter(ArrayWeekDayFormatter(resources.getTextArray(R.array.custom_weekdays)))
+
+            // 헤더 폰트 설정
+            setHeaderTextAppearance(R.style.Pome_SemiBold_16)
+
+            // 선택시 드로어블 적용
+            addDecorators(DayDecorator(requireContext()))
+        }
+
+        calendarBottomSheetDialogBinding.calendarMcv.setTitleFormatter(object : TitleFormatter{
+            override fun format(day: CalendarDay?): CharSequence {
+                day?.let {
+                    val calendarElement = it.date.toString().split("-")
+                    return "${calendarElement[0]}년 ${calendarElement[1]}월"
+                } ?: return ""
+            }
+        })
+    }
+}
+
+class DayDecorator(private val ctx: Context) : DayViewDecorator {
+    override fun shouldDecorate(day: CalendarDay?): Boolean {
+        return true
+    }
+
+    override fun decorate(view: DayViewFacade?) {
+        ContextCompat.getDrawable(ctx, R.drawable.selector_calendar)
+            ?.let { view?.setSelectionDrawable(it) }
     }
 }
