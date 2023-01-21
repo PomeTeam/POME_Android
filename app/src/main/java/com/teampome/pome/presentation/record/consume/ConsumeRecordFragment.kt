4e107d371f78acedf2389db0f042a5f7d283b67a
hanseuk-Choi <hschoi5542@gmail.com>
2023-01-21 16:50:18 +0900

    feat : pomeEditText 구현 및 적용

--- app/src/main/java/com/teampome/pome/presentation/record/consume/ConsumeRecordFragment.kt
+++ app/src/main/java/com/teampome/pome/presentation/record/consume/ConsumeRecordFragment.kt
@@ -81,16 +81,16 @@ class ConsumeRecordFragment : BaseFragment<FragmentConsumeRecordBinding>(R.layou
             moveToConsumeEmotion()
         }
 
-        // 글자수 체크
-        binding.consumeRecordContentAet.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-            override fun afterTextChanged(p0: Editable?) {
-                binding.consumeRecordContentCharacterCountAtv.text = "${p0?.length}/150"
-            }
-        })
+//        // 글자수 체크
+//        binding.consumeRecordContentAet.addTextChangedListener(object : TextWatcher {
+//            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//            }
+//            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+//            }
+//            override fun afterTextChanged(p0: Editable?) {
+//                binding.consumeRecordContentCharacterCountAtv.text = "${p0?.length}/150"
+//            }
+//        })
     }
 
     private fun makeGoalBottomSheetDialog() {
