0c6506d5291acfb5d78a94da2e46c7b695672d5a
hanseuk-Choi <hschoi5542@gmail.com>
2023-01-24 01:39:51 +0900

    fix : device size deprecated 수정

--- app/src/main/java/com/teampome/pome/util/CommonUtil.kt
+++ app/src/main/java/com/teampome/pome/util/CommonUtil.kt
@@ -1,3 +1,5 @@
+@file:Suppress("DEPRECATION")
+
 package com.teampome.pome.util
 
 import android.app.Activity
@@ -6,7 +8,9 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.Point
 import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.view.LayoutInflater
+import android.view.WindowInsets
 import android.view.WindowManager
 import android.view.WindowManager.LayoutParams
 import android.view.inputmethod.InputMethodManager
@@ -87,14 +91,30 @@ object CommonUtil {
     }
 
     // device size
-    fun getDeviceSize(context: Context): IntArray {
+    private fun getDeviceSize(context: Context): IntArray {
         val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        val display = windowManager.defaultDisplay
-        val size = Point()
 
-        display.getSize(size)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val windowMetrics = windowManager.currentWindowMetrics
+            val windowInsets = windowMetrics.windowInsets
 
-        return intArrayOf(size.x, size.y)
+            val insets = windowInsets.getInsetsIgnoringVisibility(
+                WindowInsets.Type.navigationBars() or WindowInsets.Type.displayCutout()
+            )
+            val insetsWidth = insets.right + insets.left
+            val insetsHeight = insets.top + insets.bottom
+
+            val bounds = windowMetrics.bounds
+
+            return intArrayOf(bounds.width() - insetsWidth, bounds.height() - insetsHeight)
+        } else {
+            val display = windowManager.defaultDisplay
+            val size = Point()
+
+            display?.getSize(size)
+
+            return intArrayOf(size.x, size.y)
+        }
     }
 
     fun dateToLocalDate(date: Date) : LocalDate {
