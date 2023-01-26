c178f0ec03730825530155fc22f0447799e78d10
hanseulchoi <lotte.hanseul@gmail.com>
2023-01-26 17:23:52 +0900

    feat : ApiRequestFlow 추가

---
+++ app/src/main/java/com/teampome/pome/util/base/ApiRequestFlow.kt
@@ -0,0 +1,34 @@
+package com.teampome.pome.util.base
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.withTimeoutOrNull
+import retrofit2.Response
+
+fun<T> apiRequestFlow(call: suspend () -> Response<T>): Flow<ApiResponse<T>> = flow {
+    // 먼저, Loading 상태 표현
+    emit(ApiResponse.Loading)
+
+    // 2초 TimeOut
+    withTimeoutOrNull(20000L) {
+        val response = call()
+
+        try {
+            if(response.isSuccessful) {
+                response.body()?.let { data ->
+                    emit(ApiResponse.Success(data))
+                }
+            } else {
+                response.errorBody()?.let { error ->
+                    error.close()
+
+                    // error emit 과정 필요
+                }
+            }
+        } catch (e: Exception) {
+            emit(ApiResponse.Failure(e.message ?: e.toString(), 400))
+        }
+    } ?: emit(ApiResponse.Failure("Timeout! Please try again.", 408))
+}.flowOn(Dispatchers.IO)
