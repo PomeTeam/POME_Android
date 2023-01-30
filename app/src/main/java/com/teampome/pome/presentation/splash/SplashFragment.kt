895ce18477a873b32e28c9e1cc7c3b11529a22ff
hanseulchoi <lotte.hanseul@gmail.com>
2023-01-31 08:53:30 +0900

    feat : Splash 작업 주석 설정

--- app/src/main/java/com/teampome/pome/presentation/splash/SplashFragment.kt
+++ app/src/main/java/com/teampome/pome/presentation/splash/SplashFragment.kt
@@ -6,12 +6,21 @@ import androidx.navigation.fragment.findNavController
 import com.teampome.pome.R
 import com.teampome.pome.util.base.BaseFragment
 import com.teampome.pome.databinding.FragmentSplashBinding
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.*
 
 // Todo : Android 12부터 Splash가 default 되어있기 때문에 따로 커스텀이 필요
 
+@AndroidEntryPoint
 class SplashFragment : BaseFragment<FragmentSplashBinding>(R.layout.fragment_splash){
 
+    /*
+     *  네트워크 확인
+     *  회원 확인
+     *  (UserManager의 phonNum정보가 없는가? 혹은 로그인 시 정상적으로 토큰이 떨어지는가?)
+     *  회원이면 -> recordView
+     *  비회원이면 -> RegisterView
+     */
     private var isFirstLogin = true
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
