7c5c276dfb6173ddf957c84593f23c2d4b374130
hanseulchoi <lotte.hanseul@gmail.com>
2023-03-02 17:26:51 +0900

    feat : backup RecordPagingSource

--- app/src/main/java/com/teampome/pome/repository/record/paging/RecordPagingSource.kt
+++ app/src/main/java/com/teampome/pome/repository/record/paging/RecordPagingSource.kt
@@ -1,40 +1,59 @@
-//package com.teampome.pome.repository.record.paging
-//
-//import androidx.paging.PagingSource
-//import androidx.paging.PagingState
-//import com.teampome.pome.model.RecordData
-//import com.teampome.pome.model.base.BasePomeResponse
-//import com.teampome.pome.network.RecordService
-//import retrofit2.HttpException
-//import java.io.IOException
-//import javax.inject.Inject
-//
-//const val DEFAULT_PAGE_INDEX = 1
-//
-//class RecordPagingSource @Inject constructor(
-//    private val service: RecordService,
-//    private val userId: String
-//) : PagingSource<Int, BasePomeResponse<RecordData>>() {
-//    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, BasePomeResponse<RecordData>> {
-//        val page = params.key ?: DEFAULT_PAGE_INDEX
-//
-//        return try {
-//            val response = service.getRecordDataByUserId(userId, page, params.loadSize).body()
-//
-//            LoadResult.Page(
-//                response, prevKey = if (page == DEFAULT_PAGE_INDEX) null else page - 1,
-//                nextKey = if (response.isEmpty()) null else page + 1
-//            )
-//        } catch(e : IOException) {
-//            return LoadResult.Error(e)
-//        } catch(e : HttpException) {
-//            return LoadResult.Error(e)
-//        }
-//    }
-//
-//    override fun getRefreshKey(state: PagingState<Int, BasePomeResponse<RecordData>>): Int? {
-//        return state.anchorPosition?.let { anchorPosition ->
-//                state.closestItemToPosition(anchorPosition)?.
-//        }
-//    }
-//}
+package com.teampome.pome.repository.record.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.teampome.pome.model.RecordData
+import com.teampome.pome.network.RecordService
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import retrofit2.HttpException
+import java.io.IOException
+
+class RecordPagingSource @AssistedInject constructor(
+    private val service: RecordService,
+    @Assisted("goalId") private val goalId: Int // Assisted는 Hilt를 통해 주입되지 않는다는 Annotation
+) : PagingSource<Int, RecordData>() {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, RecordData> {
+        return try {
+            val pageNumber = params.key ?: 0
+            val pageSize = params.loadSize
+            val response = service.getRecordByGoalId(goalId, pageNumber, pageSize)
+
+            if (response.isSuccessful) {
+                val baseAllData = response.body()?.data
+                val items = baseAllData?.content?.filterNotNull() ?: emptyList()
+                val prevKey = if (pageNumber == 0) null else pageNumber - 1
+                val nextKey = if (items.isEmpty() || response.body()?.data?.empty == true) null else pageNumber + 1
+
+                LoadResult.Page(items, prevKey, nextKey)
+            } else {
+                LoadResult.Error(Exception("Error loading data"))
+            }
+        } catch(e : IOException) {
+            return LoadResult.Error(e)
+        } catch(e : HttpException) {
+            return LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, RecordData>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+
+    @com.squareup.inject.assisted.AssistedInject.Factory
+    interface Factory {
+        fun create(goalId: Int): RecordPagingSource
+    }
+
+    companion object {
+        fun provideFactory(
+            assistedFactory: Factory,
+            goalId: Int
+        ): () -> RecordPagingSource = {
+            assistedFactory.create(goalId)
+        }
+    }
+}
