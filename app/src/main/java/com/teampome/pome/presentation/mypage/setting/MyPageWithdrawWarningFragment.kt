e06daa6a85fd46bff902e103805b7dc3e4ea8402
hanseuk-Choi <hschoi5542@gmail.com>
2023-06-14 00:41:44 +0900

    [feat] : myPageWithDrawWarning 다이얼로그 구현

--- app/src/main/java/com/teampome/pome/presentation/mypage/setting/MyPageWithdrawWarningFragment.kt
+++ app/src/main/java/com/teampome/pome/presentation/mypage/setting/MyPageWithdrawWarningFragment.kt
@@ -1,10 +1,14 @@
 package com.teampome.pome.presentation.mypage.setting
 
+import android.app.Dialog
+import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.teampome.pome.R
 import com.teampome.pome.databinding.FragmentMypageWithdrawWarningBinding
+import com.teampome.pome.databinding.PomeTitleYesNoDialogBinding
 import com.teampome.pome.util.base.BaseFragment
+import com.teampome.pome.util.common.CommonUtil
 import com.teampome.pome.viewmodel.mypage.MyPageWithdrawWarningViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,7 +18,13 @@ class MyPageWithdrawWarningFragment : BaseFragment<FragmentMypageWithdrawWarning
 
     private val viewModel: MyPageWithdrawWarningViewModel by viewModels()
 
+    // 탈퇴 완료 Dialog
+    private lateinit var withdrawDialog: Dialog
+    private lateinit var withdrawDialogBinding: PomeTitleYesNoDialogBinding
+
     override fun initView() {
+        makeWithdrawDialog()
+
         binding.viewModel = viewModel
     }
 
@@ -26,7 +36,36 @@ class MyPageWithdrawWarningFragment : BaseFragment<FragmentMypageWithdrawWarning
 
         // 탈퇴하기
         binding.withdrawOkayBtn.setOnClickListener {
-
+            withdrawDialog.show()
         }
+    }
+
+    private fun makeWithdrawDialog() {
+        withdrawDialog = Dialog(requireContext())
+        withdrawDialogBinding = PomeTitleYesNoDialogBinding.inflate(layoutInflater, null, false)
+
+        withdrawDialog.setContentView(withdrawDialogBinding.root)
+        withdrawDialog.setCancelable(false)
+
+        CommonUtil.makePomeDialog(withdrawDialog)
+
+        withdrawDialogBinding.pomeTitleTv.text = "탈퇴가 완료되었습니다."
+
+        withdrawDialogBinding.pomeNoTv.visibility = View.GONE
+
+        withdrawDialogBinding.pomeYesTv.apply {
+            text = "확인"
+
+            setOnClickListener {
+                withdrawDialog.dismiss()
+
+                withdrawProcess()
+            }
+        }
+    }
+
+    private fun withdrawProcess() {
+//        viewModel.removeAllUserData()
+        // splashview 이동
     }
 }
--- app/src/main/java/com/teampome/pome/viewmodel/mypage/MyPageWithdrawWarningViewModel.kt
+++ app/src/main/java/com/teampome/pome/viewmodel/mypage/MyPageWithdrawWarningViewModel.kt
@@ -1,9 +1,11 @@
 package com.teampome.pome.viewmodel.mypage
 
+import androidx.lifecycle.viewModelScope
 import com.teampome.pome.util.base.BaseViewModel
 import com.teampome.pome.util.token.UserManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
@@ -14,5 +16,11 @@ class MyPageWithdrawWarningViewModel @Inject constructor(
 
     val userName = runBlocking {
         userManager.getUserNickName().first()
+    }
+
+    fun removeAllUserData() {
+        viewModelScope.launch {
+            userManager.deleteAllUserData()
+        }
     }
 }
